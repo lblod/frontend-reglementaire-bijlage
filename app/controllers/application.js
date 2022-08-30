87e453a2787c654d4a658a6d95a12dcbeb6e1689
Elena Poelman <elena.poelman@redpencil.io>
2022-08-30 16:05:47 +0200

    check whether environmentName is explicitely set

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { getOwner } from '@ember/application';
 
 export default class ApplicationController extends Controller {
   @service store;
@@ -10,5 +11,17 @@ export default class ApplicationController extends Controller {
   @action
   logout() {
     this.session.invalidate();
+  }
+
+  get environmentName() {
+    return getOwner(this).resolveRegistration('config:environment')
+      .environmentName;
+  }
+
+  get showEnvironment() {
+    return (
+      this.environmentName !== '' &&
+      this.environmentName !== '{{ENVIRONMENT_NAME}}'
+    );
   }
 }
