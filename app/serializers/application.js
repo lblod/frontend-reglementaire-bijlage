4917804447fc3412f4b97a706e7dbadd841b767e
Elena Poelman <elena.poelman@redpencil.io>
2022-10-17 16:08:28 +0200

    remove call to super in serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -5,10 +5,6 @@ import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 export default class ApplicationSerializer extends JSONAPISerializer.extend(
   DataTableSerializerMixin
 ) {
-  normalizeResponse(store, model, payload, id, requestType) {
-    return super.normalizeResponse(store, model, payload, id, requestType);
-  }
-
   serializeAttribute(snapshot, json, key, attributes) {
     if (key !== 'uri')
       super.serializeAttribute(snapshot, json, key, attributes);
