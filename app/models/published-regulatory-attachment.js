e6bb9fd6b2d25a8ff0dbf2be678afa67042fc7d7
Elena Poelman <elena.poelman@redpencil.io>
2022-11-02 16:45:14 +0100

    move validThrough property to published-regulatory-attachment

--- app/models/published-regulatory-attachment.js
+++ app/models/published-regulatory-attachment.js
@@ -2,6 +2,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class PublishedRegulatoryAttachment extends Model {
   @belongsTo('editor-document') derivedFrom;
+  @attr validThrough;
   @attr name;
   @attr format;
   @attr size;
