b77ed9aab9059ea24adec543c2408ca54b96d4d4
Ruben <ruben.de.facq@redpencil.io>
2023-06-27 15:05:05 +0200

    restructure confirm-route-leave to use an actual modal + add "unsaved changes" modal to codelist form

--- app/components/confirm-route-leave.js
+++ app/components/confirm-route-leave.js
@@ -1,9 +1,23 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import isLoadingRoute from '../utils/is-loading-route';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
+/**
+ * Show a modal during a route transition that stops the transition if cancel is pressed.
+ * By default it will show a message to confirm to discard unsaved changes.
+ * @message (optional) the message to show in the modal
+ * @confirmMessage (optional) the text to show in the confirm button
+ * @enabled Should the modal be shown on a transition
+ * @onConfirm (optional) hook when confirmation is clicked
+ * @onCancel (optional) hook when cancellation is clicked
+ */
 export default class ConfirmRouteLeaveComponent extends Component {
   @service router;
+  isConfirmedTransition = false;
+  @tracked showConfirmModal = false;
+  lastTransition = null;
 
   constructor(...args) {
     super(...args);
@@ -18,35 +32,55 @@ export default class ConfirmRouteLeaveComponent extends Component {
     this.router.off('routeWillChange', this, this.confirm);
   }
 
-  onConfirm(transition) {
+  @action
+  onConfirm() {
     if (this.args.onConfirm) {
-      this.args.onConfirm(transition);
+      this.args.onConfirm(this.lastTransition);
     }
+    this.isConfirmedTransition = true;
+    this.lastTransition.retry().then(() => {
+      this.reset();
+    });
   }
 
-  onCancel(transition) {
+  @action
+  onCancel() {
     if (this.args.onCancel) {
-      this.args.onCancel(transition);
-    } else {
-      transition.abort();
+      this.args.onCancel(this.lastTransition);
+    }
+    this.reset();
+  }
 
-      if (window.history) {
-        window.history.forward();
-      }
+  abortTransition(transition) {
+    transition.abort();
+    if (window.history) {
+      window.history.forward();
     }
   }
 
   confirm(transition) {
-    if (transition.isAborted || isLoadingRoute(transition.to)) {
+    if (
+      this.isConfirmedTransition ||
+      !this.args.enabled ||
+      transition.isAborted ||
+      isLoadingRoute(transition.to)
+    ) {
       return;
     }
-    if (this.args.enabled) {
-      if (window.confirm(this.args.message)) {
-        this.onConfirm(transition);
-      } else {
-        this.onCancel(transition);
-      }
+
+    this.abortTransition(transition);
+
+    if (this.showConfirmModal) {
+      return;
     }
+    this.lastTransition = transition;
+    this.showConfirmModal = true;
+  }
+
+  reset() {
+    this.showConfirmModal = false;
+    this.isConfirmedTransition = false;
+    this.lastTransition = null;
   }
 
   willDestroy(...args) {
