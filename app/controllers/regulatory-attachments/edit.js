8c91cfb6f5744599c6e0795365fd729cf78797fe
Deniss Kozickis <deniss.kozickis@gmail.com>
2023-08-23 18:03:38 +0300

    GN-4322: Query `raw-sparql` for snippets

--- app/controllers/regulatory-attachments/edit.js
+++ app/controllers/regulatory-attachments/edit.js
@@ -216,7 +216,7 @@ export default class EditController extends Controller {
         interactive: true,
       },
       snippet: {
-        endpoint: '/sparql',
+        endpoint: '/raw-sparql',
       },
     };
   }
