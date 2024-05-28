689bce9008ae03b05c1a6416daaae881f0611ccc
Oscar <lagartoverde97@gmail.com>
2024-05-28 15:19:08 +0200

    made possible to override the confirm modal

--- app/controllers/regulatory-attachments/edit.js
+++ app/controllers/regulatory-attachments/edit.js
@@ -325,6 +325,9 @@ export default class EditController extends Controller {
     this.router.transitionTo(
       'regulatory-attachments.publish',
       this.model.documentContainer.id,
+      {queryParams: {
+        overrideConfirm: true
+      }}
     );
   });
 
