2ae678d332c298565767ea80bd7d30c6112fa04e
Elena Poelman <elena.poelman@redpencil.io>
2024-12-13 21:17:07 +0100

    set-up ember-template-imports + recommended eslint and prettier setups

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,6 +48,16 @@ module.exports = {
       extends: ['plugin:n/recommended'],
     },
     {
+      files: ['**/*.gjs'],
+      parser: 'ember-eslint-parser',
+      plugins: ['ember'],
+      extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended',
+        'plugin:ember/recommended-gjs',
+      ],
+    },
+    {
       // test files
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
