0231ce24b712835d054a87b96c79ba2400031be9
Elena Poelman <elena.poelman@redpencil.io>
2025-01-07 10:22:20 +0100

    add missing attributes to task model

--- app/models/task.js
+++ app/models/task.js
@@ -3,4 +3,7 @@ import Model, { attr } from '@ember-data/model';
 export default class TaskModel extends Model {
   @attr uri;
   @attr status;
+  @attr operation;
+  @attr created;
+  @attr modified;
 }
