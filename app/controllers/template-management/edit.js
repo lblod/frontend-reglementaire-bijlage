c597a2c5940b594b64777cd65aa9feb8afde195d
Oscar <lagartoverde97@gmail.com>
2025-03-13 13:10:56 +0100

    Revert "linting"
    This reverts commit 8cd01492846c55bd60b6842ebb401675bae35cf5.

--- app/controllers/template-management/edit.js
+++ app/controllers/template-management/edit.js
@@ -284,6 +284,7 @@ export default class TemplateManagementEditController extends Controller {
     );
   }
   get config() {
+    const env = getOwner(this).resolveRegistration('config:environment');
     return {
       tableOfContents: {
         scrollContainer: () =>
